684d2720006c304d034952152a800fb4288706e6
unknown <hamtah112@gmail.com>
2023-11-12 10:37:12 -0800

    front

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -10,11 +10,28 @@ return(
         <h1>djakldjk</h1>
     </Grid>
     </Grid>
-    <Grid container spacing={0}>
-        <Grid item lg ={6} md={6} sm={12} xs={12}>
+    <Grid container spacing={0} className='second'>
+        <Grid item className='right' lg ={6} md={6} sm={12} xs={12}>
 
         </Grid>
         <Grid item lg ={6} md={6} sm={12} xs={12}>
+
+        </Grid>
+
+    </Grid>
+    <Grid container spacing={0} className='second'>
+        <Grid item className='right' lg ={4} md={4} sm={12} xs={12}>
+
+        </Grid>
+        <Grid item lg ={8} md={8} sm={12} xs={12}>
+
+        </Grid>
+
+    </Grid><Grid container spacing={0} className='second'>
+        <Grid item className='right' lg ={8} md={8} sm={12} xs={12}>
+
+        </Grid>
+        <Grid item lg ={4} md={4} sm={12} xs={12}>
 
         </Grid>
 
