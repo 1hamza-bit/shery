bd05a11c5c5c9767da58f6bc74c84bace18b1d47
unknown <hamtah112@gmail.com>
2023-11-25 23:46:25 -0800

    saerch

--- src/Components/Portfolio/index.js
+++ src/Components/Portfolio/index.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import styles from './index.module.scss'
-import { Button, Grid } from '@mui/material';
+import { Button, Grid, TextField } from '@mui/material';
 import Footer from '../Layout/footer';
 import Header from '../Layout/header';
 // import axios from 'axios';
 // import { CSSTransition } from 'react-transition-group';
 // import { AnimatePresence, motion, useAnimation } from 'framer-motion';
 import green from '../../Assets/png green.png'
+import { debounce } from 'lodash';
 
 const projectData = [
   {
@@ -59,7 +60,7 @@ const Projects = () => {
     hidden: { opacity: 0, y: -100 },
     visible: { opacity: 1, y: 0 },
   };
-  const [data, setData] = useState([]);
+  const [data, setData] = useState(projectData);
   const [searchTerm, setSearchTerm] = useState('');
   const [searchResults, setSearchResults] = useState(projectData);
   const [data1, setData1] = useState([]);
@@ -88,25 +89,103 @@ const Projects = () => {
 
   const handleTagClick = (selectedTag) => {
     const filteredData = projectData.filter((item) => item.tag === selectedTag);
+    if (selectedTag === "clear"){
+      setData(projectData)
+    }
+    else{
     setData(filteredData);
+    }
   };
 
-  const handleSearch = () => {
-    debugger
-    if (searchTerm === "" ){
-    setSearchResults(projectData);
-    }
-    else {
-    const results = projectData.filter(item =>
-      item.title.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-    setSearchResults(results);
-    }
-  };
+  // const handleSearch = (event) => {
+  //     debugger
+  //   if (searchTerm.length >= 2) {
+  //     // Set a delay of, for example, 500 milliseconds before triggering the search
+  //     setTimeout(() => {
+  //       debugger
+  //       // Perform the search operation here, for example, call an API or update the state
+  //       console.log(`Searching for: ${searchTerm}`);
+  //       const results = projectData.filter(item =>
+  //         item.title.toLowerCase().includes(searchTerm.toLowerCase())
+  //       );
+  
+  //       // Assuming you want to update some state when the search is done
+  //       if (results.length === 0) {
+  //         // Show an alert if nothing is found
+  //         alert("Nothing found");
+  //       }
+  
+  //       // Update the state with either the search results or the original data
+  //       setData(results.length > 0 ? results : projectData);
+  //     }, 500);
+  //   } else {
+  //     // If the search term is less than 2 characters, revert to the original data
+  //     setData(projectData);
+  //   }
+  
+  // };
+
+  useEffect(() => {
+    const debouncedSearch = debounce((term) => {
+      if (term.length >= 2) {
+        // Perform the search operation here, for example, call an API or update the state
+        console.log(`Searching for: ${term}`);
+        const results = projectData.filter(item =>
+          item.title.toLowerCase().includes(term.toLowerCase())
+        );
+
+        // Assuming you want to update some state when the search is done
+        if (results.length === 0) {
+          // Show an alert if nothing is found
+          alert("Nothing found");
+        }
+
+        // Update the state with either the search results or the original data
+        setData(results.length > 0 ? results : projectData);
+      } else {
+        // If the search term is less than 2 characters, revert to the original data
+        setData(projectData);
+      }
+    }, 500);
+
+    debouncedSearch(searchTerm);
+
+    // Cleanup the debounced function on component unmount
+    return () => debouncedSearch.cancel();
+  }, [searchTerm, projectData]);
+
+  // const debouncedSearch = debounce((term) => {
+  //   debugger
+  //   if (term.length >= 2) {
+  //     // Perform the search operation here, for example, call an API or update the state
+  //     console.log(`Searching for: ${term}`);
+  //     const results = projectData.filter(item =>
+  //       item.title.toLowerCase().includes(term.toLowerCase())
+  //     );
+
+  //     // Assuming you want to update some state when the search is done
+  //     if (results.length === 0) {
+  //       // Show an alert if nothing is found
+  //       alert("Nothing found");
+  //     }
+
+  //     // Update the state with either the search results or the original data
+  //     setData( projectData);
+  //   } else {
+  //     // If the search term is less than 2 characters, revert to the original data
+  //     setData(projectData);
+  //   }
+  // }, 500);
+
+  // const handleSearch = (event) => {
+  //   // Check if the Enter key is pressed (key code 13)
+  //     debouncedSearch(searchTerm);
+    
+  // };
 
   const handleChange = (e) => {
     setSearchTerm(e.target.value);
-    handleSearch(); // Trigger search on every change
+    // handleSearch(); // Trigger search on every change
   };
 
 
@@ -115,7 +194,7 @@ const Projects = () => {
     <section id="projects" className={styles.projects}>
       <div className={styles.overlay}>
         <Header />
-      <div className={styles.secondoverlay}>
+      <div className={`${styles.secondoverlay} `}>
 
      {/* <motion.div
           initial="hidden"
@@ -123,14 +202,21 @@ const Projects = () => {
           variants={gridItemVariants}
           transition={{ duration: 0.9, delay: 0.9 }}
         >  */}
+        <div className='dfi'>
       <h1>Our Projects   </h1>
       <input
+      variant='standard'
         type="text"
+        size='large'
         placeholder="Search by title"
         value={searchTerm}
-        onChange={handleChange}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        // onKeyDown={handleSearch}
+        className={styles.search}
       />
+      </div>
       <div className={styles['project-tags']}>
+      <Button className={styles.tag} onClick={() => handleTagClick("clear")}>Clear</Button>  
       {tags.map((tag, index) => (
         <Button
           key={index}
@@ -146,7 +232,7 @@ const Projects = () => {
         <Grid container spacing={2}>
       
    
-        {searchResults.map((project, index) => (
+        {data.map((project, index) => (
       
           <Grid item lg={4} md={6} sm={11}>
                  {/* <motion.div
